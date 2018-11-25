49fbe76219550cebb40b99382ae57c2fcd443767
Alejandro Del Prado <31344555+AlexDelPrado@users.noreply.github.com>
2018-11-25 23:54:21 +0100

    Update AA3v2.c

--- AA3v2.c
+++ AA3v2.c
@@ -5,15 +5,14 @@
 #define FINSEC '.'
 
 /*Declaracion de tipos*/
-//typedef enum {false, true} bool; /*Declaracion tipo booleano*/
-
+typedef enum {false, true} bool; /*Declaracion tipo booleano*/
 typedef struct{
     char vocal;
     int numVeces;
 }tFrecUnaVocal;
 
 typedef struct{
-    tFrecUnaVocal frecuencias[NUMVOCALES];
+    tFrecUnaVocal frecuencias[NUMVOCALES+1]; /*Tengo que mirar esto. Por que con 6 si y 5 no*/
     int nVocales;
 }tFrecVocales;
 
@@ -32,12 +31,13 @@ int main(){
     char car;
 
     inicilizarFrecuencias(&frecVocales);
-    scanf("%c", &car);
+    scanf(" %c", &car);
     while (car != FINSEC){
         procesarCaracter(car, &frecVocales);
-        scanf("%c", &car);
+        scanf(" %c", &car);
     }
     mostrarResultado(frecVocales);
+    //return 0;
 }
 
 void inicilizarFrecuencias(tFrecVocales *frecVocales){
@@ -47,21 +47,22 @@ void inicilizarFrecuencias(tFrecVocales *frecVocales){
 void procesarCaracter(char c, tFrecVocales *frecVocales){
     int i;
     bool encontrado;
-    
+
     if (esVocal(c)){
+        encontrado = false;
         i = 1;
-        while ((i <= frecVocales->nVocales) && (!encontrado)){
+        while ((i <= frecVocales->nVocales) && !encontrado){
             encontrado = c == frecVocales->frecuencias[i].vocal;
             if (!encontrado){
                 i++;
             }
         }
         if (!encontrado){
-            frecVocales->nVocales++;
-            frecVocales->frecuencias[frecVocales->nVocales].vocal = c;
+            frecVocales->nVocales = frecVocales->nVocales + 1;/*Aqui le estamos diciendo que sume una posicion a la tupla. Por eso empieza en el 1*/
+            frecVocales->frecuencias[frecVocales->nVocales].vocal = c;/*en vez de en el cero*/
             frecVocales->frecuencias[frecVocales->nVocales].numVeces = 1;
         }else{
-            frecVocales->frecuencias[i].numVeces++;
+            frecVocales->frecuencias[i].numVeces = frecVocales->frecuencias[i].numVeces + 1;
         }
     }
 }
@@ -70,14 +71,14 @@ bool esVocal(char c){
     return ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 }
 
-void mostrarResultado(tFrecVocales frecVocales){
+void mostrarResultado(const tFrecVocales frecVocales){
     int i, totalVocales;
 
     totalVocales = cuentaVocales(frecVocales);
     printf("Total de vocales introducidas: %d\n", totalVocales);
     for(i=1;i<=frecVocales.nVocales;i++){
         if (totalVocales != 0){
-            printf("El %% de aparicion de la %c es del %d", frecVocales.frecuencias[i].vocal, (frecVocales.frecuencias[i].numVeces * (100/totalVocales)));
+            printf("El %% de aparicion de la %c es del %d", frecVocales.frecuencias[i].vocal, (frecVocales.frecuencias[i].numVeces * 100)/totalVocales);
         }else{
             printf("%d", 0);
         }
@@ -87,7 +88,7 @@ void mostrarResultado(tFrecVocales frecVocales){
 
 int cuentaVocales(tFrecVocales frecVocales){
     int total, i;
-    
+
     total = 0;
     for(i=1;i<=frecVocales.nVocales;i++){
         total = total + frecVocales.frecuencias[i].numVeces;
